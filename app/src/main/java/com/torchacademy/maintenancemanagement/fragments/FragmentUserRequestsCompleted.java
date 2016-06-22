ecc5e884b7485ebf0661526edd3f91d5c40c8080
ahmad.masri <ase27@mail.aub.edu>
2016-06-22 05:22:46 -0700

    change profile icon

---
+++ app/src/main/java/com/torchacademy/maintenancemanagement/fragments/FragmentUserRequestsCompleted.java
@@ -0,0 +1,28 @@
+package com.torchacademy.maintenancemanagement.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.torchacademy.maintenancemanagement.R;
+
+/**
+ * Created by ahmad.masri on 6/22/2016.
+ */
+public class FragmentUserRequestsCompleted extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user_request_completed, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
