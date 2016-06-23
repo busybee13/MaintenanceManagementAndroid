e1057f672971582adfd1a4677658837b4600e187
ahmad.masri <ase27@mail.aub.edu>
2016-06-23 02:35:41 -0700

    image to imagebutton

--- app/src/main/java/com/torchacademy/maintenancemanagement/fragments/FragmentUserRequestsAll.java
+++ app/src/main/java/com/torchacademy/maintenancemanagement/fragments/FragmentUserRequestsAll.java
@@ -19,6 +19,7 @@ public class FragmentUserRequestsAll extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_user_request_all, container, false);
+
     }
 
 
